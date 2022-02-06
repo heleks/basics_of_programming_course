cf48f24fea2c08a8b828db5ed87c6fd34da730b2
heleks <okokok12378@gmail.com>
2022-02-06 11:29:02 -0800

    append access functions

--- core.c
+++ core.c
@@ -31,10 +31,44 @@ void test_popBack_notEmptyVector( ) {
     assert( v.capacity == 1 );
 }
 
+void test_atVector_notEmptyVector1( ) {
+    vector v = createVector( 1 );
+    pushBack( &v , 0 );
+
+    assert( v.data == atVector( v , 0 ) );
+}
+
+void test_atVector_notEmptyVector2( ) {
+    vector v = createVector( 2 );
+    pushBack( &v , 0 );
+    pushBack( &v , 1 );
+
+    assert( v.data + 1 == atVector( v , 1 ) );
+}
+
+void test_back_oneElementInVector( ) {
+    vector v = createVector( 2 );
+    pushBack( &v , 0 );
+    pushBack( &v , 1 );
+
+    assert( v.data[ v.size - 1 ] == back( v ) );
+}
+
+void test_front_oneElementInVector( ) {
+    vector v = createVector( 1 );
+    pushBack( &v , 0 );
+
+    assert( v.data[ v.size - 1 ] == front( v ) );
+}
+
 void test( ) {
     test_pushBack_emptyVector( );
     test_pushBack_fullVector( );
     test_popBack_notEmptyVector( );
+    test_atVector_notEmptyVector1( );
+    test_atVector_notEmptyVector2( );
+    test_back_oneElementInVector( );
+    test_front_oneElementInVector( );
 }
 
 int main( ) {
--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -67,3 +67,21 @@ void popBack( vector* v ) {
 	
 	v->size--;
 }
+
+
+int* atVector( vector* v , size_t index ) {
+	if ( index > v->size ) {
+		fprintf( stderr , "IndexError: a[ index ] is not exists" );
+		exit( 1 );
+	}
+
+	return v->data + index;
+}
+
+int* back( vector* v ) {
+	return atVector( v , v->size - 1 );
+}
+
+int* front( vector* v ) {
+	return atVector( v , 0 );
+}
