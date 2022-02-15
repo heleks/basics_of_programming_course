b93cf3baaf631262a2e92eb26c4b348a754f2616
heleks <okokok12378@gmail.com>
2022-02-15 14:31:56 -0800

    refactoring

---
+++ core.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include "libs/data_structures/matrix/matrix.h"
+
+
+//int main( ) {
+//	matrix m = getMemMatrix( 2 , 2 );
+//
+//	inputMatrix( m );
+//	swapColumns( m , 0 , 1 );
+//	outputMatrix( m );
+//
+//	//printf( "\n%d %d" , getMaxValuePos( m ).rowIndex , getMaxValuePos( m ).colIndex );
+//
+//	return 0;
+//}
