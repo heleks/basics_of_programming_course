fa58482c0d0cce2a0d89a64d826657442ac91a4f
heleks <okokok12378@gmail.com>
2022-03-15 17:59:47 -0700

    results

--- sorts/sorts.c
+++ sorts/sorts.c
@@ -384,3 +384,78 @@ long long _getRadixSort( int* l , int* r , int N ) {
 long long getRadixSortNComp( int* a , size_t n ) {
     return _getRadixSort( a , a + n , 8 );
 }
+
+void checkTime( void( *sortFunc )( int* , size_t ) , void( *generateFunc )( int* , size_t ) , size_t size , char* experimentName ) {
+    static size_t runCounter = 1;
+    static int innerBuffer[ 100000 ];
+    generateFunc( innerBuffer , size );
+    printf( "Run #%zu | " , runCounter++ );
+
+    printf( "Name : %s\n" , experimentName );
+    double time;
+    TIME_TEST( { sortFunc( innerBuffer, size ); } , time ) 
+        printf( "Status: " );
+
+        if ( isOrdered( innerBuffer , size ) ) {
+            printf( "OK! Time: %.3f s.\n" , time );
+            char* filename[ 256 ];
+            sprintf( filename , "%s.csv" , experimentName );
+            FILE* f = fopen( filename , "a" );
+
+            if ( f == NULL ) {
+                printf( "An error occurred when opening the file %s" , filename );
+                exit( 1 );
+            }
+
+            fprintf( f , "%zu; %.3f\n" , size , time );
+            fclose( f );
+        }
+        else {
+            printf( "Wrong!\n" );
+            outputArray( innerBuffer , size );
+            exit( 1 );
+        }
+}
+
+void doit( ) {
+    sortFunc sorts[ ] = {
+
+      {
+        radixSort,
+        "radixSort"
+      }
+    };
+
+    const unsigned numberOfFunc = ARRAY_SIZE( sorts );
+
+    generatingFunc generatingFuncs[ ] = {
+      {
+        generateRandomArray,
+        "random"
+      },
+      {
+        generateOrderedArray,
+        "ordered"
+      },
+      {
+        generateOrderedBackwards,
+        "orderedBackwards"
+      }
+    };
+
+    const unsigned numberOfGeneratingFuncs = ARRAY_SIZE( generatingFuncs );
+    for ( size_t size = 150000; size <= 190000; size += 10000 ) {
+        printf( "--------------------------------- \n" );
+        printf( "Size: %u\n" , size );
+
+        for ( int i = 0; i < numberOfFunc; ++i ) {
+            for ( int j = 0; j < numberOfGeneratingFuncs; ++j ) {
+                static char filename[ 128 ];
+                sprintf( filename , "%s_%s_time" , sorts[ i ].name , generatingFuncs[ j ].name );
+                checkTime( sorts[ i ].sort , generatingFuncs[ j ].generate , size , filename );
+            }
+        }
+
+        printf( "\n" );
+    }
+}
